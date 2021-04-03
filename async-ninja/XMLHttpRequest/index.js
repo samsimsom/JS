005930c435c3c0c3c7c141823109a965748b8465
samsimsom <samsimsom@gmail.com>
2021-04-03 20:21:37 +0300

    up

--- async-ninja/XMLHttpRequest/index.js
+++ async-ninja/XMLHttpRequest/index.js
@@ -193,3 +193,37 @@ function promiseExampleTwo() {
 
 /* -------------------------------------------------------------------------- */
 // Fetch Example (Modern)
+function fetchExampleOne() {
+  fetch("json/first.json")
+    .then((response) => {
+      console.log("resolved", response);
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log("rejected", error);
+    });
+}
+// fetchExampleOne();
+
+function fetchExamplesTwo() {
+  const getTodos = async () => {
+    const response = await fetch("json/first.json");
+    if (response.status !== 200) {
+      throw new Error("Cannot fetch data!");
+    }
+    const data = await response.json();
+    return data;
+  };
+
+  getTodos()
+    .then((data) => {
+      console.log("resolved:", data);
+    })
+    .catch((err) => {
+      console.log("rejected:", err.message);
+    });
+}
+fetchExamplesTwo();
