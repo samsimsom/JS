9d11b4496f7868f012901f76138aedee07d46fc6
samsimsom <samsimsom@gmail.com>
2021-04-03 19:16:57 +0300

    XMLHttpRequest

--- async-ninja/XMLHttpRequest/index.js
+++ async-ninja/XMLHttpRequest/index.js
@@ -10,7 +10,6 @@ function lessonOne() {
   request.open("GET", "https://jsonplaceholder.typicode.com/todos/1");
   request.send();
 }
-
 // lessonOne();
 
 function lessonTwo() {
@@ -28,7 +27,6 @@ function lessonTwo() {
   request.open("GET", "https://jsonplaceholder.typicode.com/todos/1");
   request.send();
 }
-
 // lessonTwo();
 
 function lessonThree() {
@@ -63,7 +61,6 @@ function lessonThree() {
   console.log(3);
   console.log(4);
 }
-
 // lessonThree();
 
 function lessonFour() {
@@ -93,4 +90,106 @@ function lessonFour() {
     }
   });
 }
-lessonFour();
+// lessonFour();
+
+function lessonFive() {
+  // Lesson Five Content
+  const getTodos = (resource, callback) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener("readystatechange", () => {
+      if (request.readyState === 4 && request.status === 200) {
+        const data = JSON.parse(request.responseText);
+        callback(undefined, data);
+      } else if (request.readyState === 4) {
+        callback("Could not fetch the data!", undefined);
+      }
+    });
+
+    request.open("GET", resource);
+    request.send();
+  };
+
+  // callback hell -------------------------------------------------------------
+  getTodos("json/first.json", (error, data) => {
+    error ? console.log(error) : console.log(data);
+    getTodos("json/second.json", (error, data) => {
+      error ? console.log(error) : console.log(data);
+      getTodos("json/third.json", (error, data) => {
+        error ? console.log(error) : console.log(data);
+      });
+    });
+  });
+}
+// lessonFive();
+
+/* -------------------------------------------------------------------------- */
+// Promise Example
+function promiseExampleOne() {
+  const getTodos = (resource) => {
+    return new Promise((resolve, reject) => {
+      const request = new XMLHttpRequest();
+
+      request.addEventListener("readystatechange", () => {
+        if (request.readyState === 4 && request.status === 200) {
+          const data = JSON.parse(request.responseText);
+          resolve(data);
+        } else if (request.readyState === 4) {
+          reject("Error getting resource");
+        }
+      });
+
+      request.open("GET", resource);
+      request.send();
+    });
+  };
+
+  getTodos("json/first.json")
+    .then((data) => {
+      console.log("promise resolved:", data);
+    })
+    .then((err) => {
+      console.log("Promise rejected:", err);
+    });
+}
+// promiseExampleOne();
+
+function promiseExampleTwo() {
+  const getTodos = (resource) => {
+    return new Promise((resolve, reject) => {
+      const request = new XMLHttpRequest();
+
+      request.addEventListener("readystatechange", () => {
+        if (request.readyState === 4 && request.status === 200) {
+          const data = JSON.parse(request.responseText);
+          resolve(data);
+        } else if (request.readyState === 4) {
+          reject("Error getting resource");
+        }
+      });
+
+      request.open("GET", resource);
+      request.send();
+    });
+  };
+
+  getTodos("json/first.json")
+    .then((data) => {
+      console.log("promise 1 resolved:", data);
+      return getTodos("json/second.json");
+    })
+    .then((data) => {
+      console.log("promise 2 resolved:", data);
+      return getTodos("json/third.json");
+    })
+    .then((data) => {
+      console.log("promise 3 resolved:", data);
+    })
+    .then((err) => {
+      console.log("Promise rejected:", err);
+    });
+}
+// promiseExampleTwo();
+
+/* -------------------------------------------------------------------------- */
+// Fetch Example (Modern)
