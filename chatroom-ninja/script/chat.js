40d3ab9bc6febc3d54af72ec3fdb23191735cfbd
samsimsom <samsimsom@gmail.com>
2021-04-19 20:21:43 +0300

    update user

--- chatroom-ninja/script/chat.js
+++ chatroom-ninja/script/chat.js
@@ -5,6 +5,7 @@ class Chatroom {
     this.room = room
     this.username = username
     this.chats = db.collection('chats')
+    this.unsub
   }
 
   async addChat(message) {
@@ -22,24 +23,29 @@ class Chatroom {
   }
 
   getChats(callback) {
-    this.chats.onSnapshot((snapshot) => {
-      snapshot.docChanges().forEach((change) => {
-        if (change.type === 'added') {
-          // update the UI
-          callback(change.doc.data())
-        }
+    this.unsub = this.chats
+      .where('room', '==', this.room)
+      .orderBy('created_at')
+      .onSnapshot((snapshot) => {
+        snapshot.docChanges().forEach((change) => {
+          if (change.type === 'added') {
+            // update the UI
+            callback(change.doc.data())
+          }
+        })
       })
-    })
+  }
+
+  updateName(username) {
+    this.username = username
+    localStorage.setItem('username', username)
+  }
+
+  updateRoom(room) {
+    this.room = room
+    console.log('room updated')
+    if (this.unsub) {
+      this.unsub()
+    }
   }
 }
-
-const chatroom = new Chatroom('gaming', 'firat')
-// console.log(chatroom)
-// chatroom
-//   .addChat('Hello Everyone')
-//   .then(() => console.log('chat added'))
-//   .catch((err) => console.log(err))
-
-chatroom.getChats((data) => {
-  console.log(data)
-})
