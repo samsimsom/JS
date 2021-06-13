d3ede4cf44f506e40024400003f55a0f33950d31
samsimsom <samsimsom@gmail.com>
2021-06-13 07:17:33 +0300

    api

---
+++ weather-app-traversy/weather.js
@@ -0,0 +1,14 @@
+console.log('--- weather.js ---')
+
+class Weather {
+  constructor() {
+    this.apikey = ''
+    this.city = city
+    this.state = state
+  }
+
+  // Fetch weather from Api
+  async getWeather() {
+    const responce = await fetch(``)
+  }
+}
